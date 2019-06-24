33243f72b244da78fb2fc875bcf8052edd63fc89
hgulamm <46911124+hgulamm@users.noreply.github.com>
2019-06-24 15:24:59 -0400

    Create Stack_LinkedList.js
    Implement Stack using LinkedList using Javascript

---
+++ Stack_LinkedList.js
@@ -0,0 +1,62 @@
+//Create linkedlist
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  //returns top of the stack
+  peek() {
+    
+    return this.top;
+    
+  }
+  //add elements to the stack
+  push(value){
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+  //removes element at the top of the stack
+  pop(){
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+myStack.peek();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+
+
+
+//Discord
+//Udemy
+//google
